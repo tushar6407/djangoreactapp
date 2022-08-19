a79b17ffe60d3cb5e5a811f6fe1559c56059210c
tushar6407 <111578385+tushar6407@users.noreply.github.com>
2022-08-19 20:25:55 +0530

    third commit

--- djangobackend/api/urls.py
+++ djangobackend/api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = [
     path('student/', views.StudentList.as_view()),
+    path('', views.StudentList.as_view()),
 ]
--- djangobackend/djangobackend/urls.py
+++ djangobackend/djangobackend/urls.py
@@ -1,7 +1,11 @@
+from django import views
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('api.urls')),
 ]
